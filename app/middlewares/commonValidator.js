2a03caf08e22069b8a680395dffcacfd35c4d867
YannDepardieu <yanndepardieu@outlook.fr>
2022-04-07 18:55:44 +0200

    commonValidator OK !

--- app/middlewares/commonValidator.js
+++ app/middlewares/commonValidator.js
@@ -1,40 +1,31 @@
-const debug = require('debug')('Validator:log');
+// const debug = require('debug')('Validator:log');
 const ApiError = require('../errors/apiError');
 const schemas = require('../schemas');
 /**
  * Generic middleware generator that validate an object fron a request propriety
  * @param {string} prop - Propriety request object name that needs validation
- * @param {Joi.object} schema - Joi module validation schema
  * @returns {Function} -
  * Send back a middleware that validates the body of the request using the parameter schema
  * Send back an error 400 if the validation fail
  */
 module.exports = (prop) => async (req, res, next) => {
     try {
-        if (res.locals.Model) {
-            debug('req.method = ', req.method);
-
-            if (req.method === 'POST') {
-                Object.keys(schemas).forEach((key) => {
-                    const regex = /^create/m;
-                    if (!regex.test(key)) {
-                        delete schemas[key];
-                    }
-                });
-            }
-            debug('schemas = ', schemas);
-
-            // la "value" on s'en fiche on la récupère pas
-            // request['body'] == request.body
-            debug(req[prop]);
-            await schemas.createUser.validateAsync(req[prop]);
+        let schema;
+        if (req.method === 'POST') {
+            Object.keys(schemas).forEach((key) => {
+                const regex = new RegExp(`^create${res.locals.Model.tableName}`, 'mi');
+                if (regex.test(key)) {
+                    schema = schemas[key];
+                }
+            });
         }
+        await schema.validateAsync(req[prop]);
 
         next();
     } catch (error) {
-        // Je dois afficher l'erreur à l'utilisateur
-        // STATUS HTTP pour une erreur de saise : 400
-        // On réabille l'erreur en suivant notre propre normalisation
+        // When the schema is not respected, an error is sended to the user
+        // STATUS HTTP for a user input error : 400
+        // We shape the error following our normalization
         next(new ApiError(error.details[0].message, { statusCode: 400 }));
     }
 };
--- app/middlewares/validator.js
+++ app/middlewares/validator.js
@@ -11,15 +11,13 @@ const ApiError = require('../errors/apiError');
  */
 module.exports = (prop, schema) => async (request, _, next) => {
     try {
-        // la "value" on s'en fiche on la récupère pas
-        // request['body'] == request.body
         debug(request[prop]);
         await schema.validateAsync(request[prop]);
         next();
     } catch (error) {
-        // Je dois afficher l'erreur à l'utilisateur
-        // STATUS HTTP pour une erreur de saise : 400
-        // On réabille l'erreur en suivant notre propre normalisation
+        // When the schema is not respected, an error is sended to the user
+        // STATUS HTTP for a user input error : 400
+        // We shape the error following our normalization
         next(new ApiError(error.details[0].message, { statusCode: 400 }));
     }
 };
