85d9af42cea99f5511d05bfbef6291c5eac5d426
YannDepardieu <yanndepardieu@outlook.fr>
2022-04-21 13:04:02 +0200

    starting to work on contraint

--- data/Check.sql
+++ data/Check.sql
@@ -7,9 +7,16 @@ ALTER TABLE "user"
 CREATE DOMAIN lat_range AS REAL CHECK (VALUE >= -90 AND VALUE <= 90);
 CREATE DOMAIN long_range AS REAL CHECK (VALUE >= -180 AND VALUE <= 180);
 
-ALTER TABLE ""
+ALTER TABLE "place"
+    ALTER COLUMN latitude TYPE lat_range,
+    ALTER COLUMN longitude TYPE long_range;
 
-                arrayLatLong[0] >= -90 &&
-                arrayLatLong[0] <= 90 &&
-                arrayLatLong[1] >= -180 &&
-                arrayLatLong[1] <= 180
+ALTER TABLE "event"
+    ALTER COLUMN latitude TYPE lat_range,
+    ALTER COLUMN longitude TYPE long_range;
+
+ALTER TABLE "event"
+    ADD CONSTRAINT event_datetime_check
+    CHECK (event_datetime ~ VALUE >= NOW())
+    ADD CONSTRAINT recall_datetime_check
+    CHECK (recall_datetime ~ VALUE <= event_datetime);
