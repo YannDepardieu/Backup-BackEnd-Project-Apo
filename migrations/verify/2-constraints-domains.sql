7cea47b17b1a6dd680df15f62bf000a22e0022e2
YannDepardieu <yanndepardieu@outlook.fr>
2022-04-21 16:06:29 +0200

    verify constraints and domains in progress

--- migrations/verify/2-constraints-domains.sql
+++ migrations/verify/2-constraints-domains.sql
@@ -3,5 +3,18 @@
 BEGIN;
 
 -- XXX Add verifications here.
+INSERT INTO "user" ("firstname", "lastname", "email", "password", "role", "notification")
+VALUES ('Hermione', 'Granger', 'hermione@hogwarts.com', 'doso9_è=+}l%ù', 'admin', true);
+
+INSERT INTO "user" ("firstname", "lastname", "email", "password", "role", "notification")
+VALUES ('Tom', 'Riddle', 'tom.riddle@hogwarts.com', '65fdfze**$"=)à)à', 'user', false);
+
+INSERT INTO "user" ("firstname", "lastname", "email", "password", "role", "notification")
+VALUES ('Potter', 'Griffindor', 'potter+gryffindor@hogwarts.com', 'gzefbuij&&²"#', 'admin', false);
+
+INSERT INTO "user" ("firstname", "lastname", "email", "password", "role", "notification")
+VALUES ('Harry', 'Potter', 'harry.potter@hogwarts.eu.com', '(è_5651.46==+*ùd', 'user', true);
+
+
 
 ROLLBACK;
